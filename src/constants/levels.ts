5833d7fa0621c40e98dbe03161747bb6a29ed00f
Mark <mark@remarkablemark.org>
2022-01-30 17:31:11 -0500

    refactor(assets): rename level3 to down

--- src/constants/levels.ts
+++ src/constants/levels.ts
@@ -1,5 +1,5 @@
+import down from '../assets/tilemaps/down.json';
 import invert from '../assets/tilemaps/invert.json';
-import level3 from '../assets/tilemaps/level3.json';
 import level4 from '../assets/tilemaps/level4.json';
 import level5 from '../assets/tilemaps/level5.json';
 import level6 from '../assets/tilemaps/level6.json';
@@ -14,7 +14,9 @@ export const levels = [
 
   // 2
   invert,
-  level3,
+
+  // 3
+  down,
   level4,
   level5,
   level6,
