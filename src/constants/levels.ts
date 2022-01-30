65cb3847675292983f711c7d14e8873758bd190a
Mark <mark@remarkablemark.org>
2022-01-30 17:36:11 -0500

    refactor(assets): rename level4 to precise

--- src/constants/levels.ts
+++ src/constants/levels.ts
@@ -1,11 +1,11 @@
 import down from '../assets/tilemaps/down.json';
 import invert from '../assets/tilemaps/invert.json';
-import level4 from '../assets/tilemaps/level4.json';
 import level5 from '../assets/tilemaps/level5.json';
 import level6 from '../assets/tilemaps/level6.json';
 import level7 from '../assets/tilemaps/level7.json';
 import level8 from '../assets/tilemaps/level8.json';
 import level10 from '../assets/tilemaps/level10.json';
+import precise from '../assets/tilemaps/precise.json';
 import start from '../assets/tilemaps/start.json';
 
 export const levels = [
@@ -17,7 +17,9 @@ export const levels = [
 
   // 3
   down,
-  level4,
+
+  // 4
+  precise,
   level5,
   level6,
   level7,
