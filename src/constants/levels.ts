b28df3129677f653c1119f19676b7fbdf1478c2e
Mark <mark@remarkablemark.org>
2022-01-30 17:16:55 -0500

    refactor(assets): rename level1 to start

--- src/constants/levels.ts
+++ src/constants/levels.ts
@@ -1,4 +1,3 @@
-import level1 from '../assets/tilemaps/level1.json';
 import level2 from '../assets/tilemaps/level2.json';
 import level3 from '../assets/tilemaps/level3.json';
 import level4 from '../assets/tilemaps/level4.json';
@@ -7,9 +6,11 @@ import level6 from '../assets/tilemaps/level6.json';
 import level7 from '../assets/tilemaps/level7.json';
 import level8 from '../assets/tilemaps/level8.json';
 import level10 from '../assets/tilemaps/level10.json';
+import start from '../assets/tilemaps/start.json';
 
 export const levels = [
-  level1,
+  // 1
+  start,
   level2,
   level3,
   level4,
