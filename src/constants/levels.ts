a64f46d21ad19a9fdda14d02ae4f30ddcdc7af62
Ben Budnevich <benbudnevich@gmail.com>
2022-01-30 00:44:56 -0500

    feat: add levels

--- src/constants/levels.ts
+++ src/constants/levels.ts
@@ -2,5 +2,18 @@ import level1 from '../assets/tilemaps/level1.json';
 import level2 from '../assets/tilemaps/level2.json';
 import level3 from '../assets/tilemaps/level3.json';
 import level4 from '../assets/tilemaps/level4.json';
+import level5 from '../assets/tilemaps/level5.json';
+import level6 from '../assets/tilemaps/level6.json';
+import level7 from '../assets/tilemaps/level7.json';
+import level10 from '../assets/tilemaps/level10.json';
 
-export const levels = [level1, level2, level3, level4] as const;
+export const levels = [
+  level1,
+  level2,
+  level3,
+  level4,
+  level5,
+  level6,
+  level7,
+  level10,
+] as const;
